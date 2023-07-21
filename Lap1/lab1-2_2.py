3d4a5a9f12c677c15c0cfeadf5b25ef9f8f6cdd8
PLAYPLAK <131369587+PLAYPLAK@users.noreply.github.com>
2023-07-21 10:13:47 +0700

    Update lab1-2_2.py

--- Lap1/lab1-2_2.py
+++ Lap1/lab1-2_2.py
@@ -9,3 +9,33 @@ from mpl_toolkits.mplot3d import Axes3D
 
 image = cv2.imread("kmitl.jpg")
 image2 = cv2.imread("chainat.jpg")
+
+# Convert Color
+rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+rgb_image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2RGB)
+
+# Set Image Size
+height, width = 300, 500
+image1 = cv2.resize(image, (width, height))
+image2 = cv2.resize(image2, (width, height))
+
+# Define the video output settings
+output_file = 'blended_video.mp4'  # Replace with the desired output file name
+frame_rate = 1  # Number of frames per second
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+
+# Create the video writer object
+video_writer = cv2.VideoWriter(output_file, fourcc, frame_rate, (width, height))
+
+weight_arr = [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
+
+for w1, w2 in zip(weight_arr, weight_arr[::-1]):
+    im_add = cv2.addWeighted(image1, w1, image2, w2, 0)
+    video_writer.write(im_add)
+
+for w2, w1 in zip(weight_arr, weight_arr[::-1]):
+    im_add = cv2.addWeighted(image1, w1, image2, w2, 0)
+    video_writer.write(im_add)
+
+
+video_writer.release()
