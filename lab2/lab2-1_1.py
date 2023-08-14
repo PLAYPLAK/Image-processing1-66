303385f68edd721888469a16750a8600c31e02c5
PLAYPLAK <131369587+PLAYPLAK@users.noreply.github.com>
2023-08-14 21:27:48 +0700

    update 14-8

--- lab2/lab2-1_1.py
+++ lab2/lab2-1_1.py
@@ -19,39 +19,38 @@ height, width = 300, 500
 image = cv2.resize(image, (width, height))
 # Define the video output settings
 output_file = 'lener_video.mp4'  # Replace with the desired output file name
-frame_rate = 1  # Number of frames per second
+frame_rate = 5  # Number of frames per second
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 video_writer = cv2.VideoWriter(output_file, fourcc, frame_rate, (width, height))
 
 print(image.shape)
 
-n = 1
-for a in range(-10, 11) :
-    print(a)
-    for b in range(0, 10) :
-        image[:, :, 0] = (1 * image[:, :, 0] ) + b
+#n = 1
+for a in range(-10, 11, 2) :
 
-    fig.add_subplot(rows, columns, n)
-    plt.imshow(image)
-    # video_writer.write(image)
-    n = n + 1
+    for b in range(0, 101, 20) :
+        image[:, :, 0] = (a * image[:, :, 0] ) + b
+        video_writer.write(image)
+    #fig.add_subplot(rows, columns, n)
+    #plt.imshow(image)
+    #video_writer.write(image)
+    #n = n + 1
 
 
-n2 = 22
-for a in range(-10, 11) :
-    print(a)
-    for b in range(0, 10) :
-        image[:, :, 1] = (20 * image[:, :, 1] ) + b
-
-    fig.add_subplot(rows, columns, n2)
-    plt.imshow(image)
-    # video_writer.write(image)
-    n2 = n2 + 1
+#n2 = 22
+for a in range(-10, 11, 2) :
+    for b in range(0, 101, 20) :
+        image[:, :, 1] = (a * image[:, :, 1] ) + b
+        video_writer.write(image)
+    #fig.add_subplot(rows, columns, n2)
+    #plt.imshow(image)
+    #video_writer.write(image)
+    #n2 = n2 + 1
 
 #print(image)
 
-#  video_writer.release()
-plt.show()
+video_writer.release()
+#plt.show()
 
 
 # combline_image = np.hstack((image, r_chanel))
