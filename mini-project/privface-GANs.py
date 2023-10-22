9b69f1202438cd8761744cb7866350d00db35dcc
PLAYPLAK <131369587+PLAYPLAK@users.noreply.github.com>
2023-10-22 13:35:07 +0700

    update

--- mini-project/privface-GANs.py
+++ mini-project/privface-GANs.py
@@ -10,54 +10,42 @@ from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
 from PIL import Image
 
+
 filenames = glob.glob("mini-project/archive/images/*.png")
 all_imgs = []
-blur_imgs = []
-
-# ... (rest of your code for loading and processing images)
+test_imgs = []
+#model face detect
 face_model = cv2.CascadeClassifier('mini-project/face-detect-model.xml')
 
-for i in range(300):
+
+
+for i in range(len(filenames)):
     img = image.load_img(filenames[i],target_size=(128, 128, 3), interpolation="nearest")
     img = np.array(img)
-    img = tf.convert_to_tensor(img, dtype=tf.float32)
-    img = (img / 127.5) - 1
+    img = img/255
     all_imgs.append(img)
 
-    b_img = Image.open(filenames[i])
-    b_img = b_img.convert('RGB')
-    b_img = np.array(b_img)
-    
+    if len(all_imgs) > (70/100) * len(filenames):
+        b_img = Image.open(filenames[i])
+        b_img = b_img.convert('RGB')
+        b_img = np.array(b_img)
+        test_imgs.append(b_img)
 
-    gray_image = cv2.cvtColor(b_img, cv2.COLOR_BGR2GRAY)
-    face_detect = face_model.detectMultiScale(gray_image)
-    for (x,y,w,h) in face_detect:
-        #blur image
-        roi_color = b_img[y:y+h, x:x+w]
-        roi_b = cv2.GaussianBlur(roi_color, (23,23), 30)
-        b_img[y:y+h, x:x+w] = roi_b
-
-    b_img = cv2.resize(b_img,(128, 128))
-    #b_img = cv2.cvtColor(b_img, cv2.COLOR_BGR2RGB)
-    b_img = tf.convert_to_tensor(b_img, dtype=tf.float32)
-    b_img = (b_img / 127.5) - 1
-    blur_imgs.append(b_img)
 
 
 all_imgs = np.array(all_imgs)
-blur_imgs = np.array(blur_imgs)
 
-print(all_imgs.shape)
-print(blur_imgs.shape)
-
-train_img,test_img = train_test_split(all_imgs,random_state=32,test_size=0.3)
+train_img, _  = train_test_split(all_imgs,random_state=32,test_size=0.3)
 train_img,val_img = train_test_split(train_img,random_state=32,test_size=0.3)
 
-train_blur,test_blur = train_test_split(blur_imgs,random_state=32,test_size=0.3)
-train_blur,val_blur = train_test_split(train_blur,random_state=32,test_size=0.3)
+Nmean = 0
+Nstd = 1
+x_train_noisy = train_img + (0.1 * np.random.normal(loc=Nmean,scale=Nstd,size=train_img.shape))
+x_val_noisy = val_img + (0.1 * np.random.normal(loc=Nmean,scale=Nstd,size=val_img.shape))
+
 
 def build_generator(input_shape):
-    gen_input = Input(shape=(128, 128, 3))
+    gen_input = Input(shape=input_shape)
     x = Conv2D(64, (3, 3), activation='relu', padding='same')(gen_input)
     x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
     x = UpSampling2D((1, 1))(x)
@@ -69,7 +57,7 @@ def build_generator(input_shape):
 
 def build_discriminator(input_shape):
     # Example architecture for the discriminator
-    disc_input = Input(shape=(128, 128, 3))
+    disc_input = Input(shape=input_shape)
     x = Conv2D(64, (3, 3), activation='relu', padding='same')(disc_input)
     x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
     output_layer = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(x)  # Output a 3-channel image
@@ -86,22 +74,23 @@ def build_discriminator(input_shape):
 input_shape = (128, 128, 3)  # Change this to match your image size
 generator = build_generator(input_shape)
 discriminator = build_discriminator(input_shape)
-#generator = load_model('generator_model.h5')
-#discriminator = load_model('discriminator_model.h5')
+#generator = load_model('generator_model.keras')
+#discriminator = load_model('discriminator_model.keras')
 
 discriminator.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.0002, beta_1=0.5), metrics=['accuracy'])
-#generator.summary()
-#discriminator.summary()
+generator.summary()
+discriminator.summary()
 
 # Create the GAN model
-discriminator.trainable = None  # Freeze the discriminator during generator training
+
+discriminator.trainable = False  # Freeze the discriminator during generator training
 gan_input = Input(shape=(128, 128, 3))
 generated_image = generator(gan_input)
 gan_output = discriminator(generated_image)
 
 gan = Model(gan_input, gan_output)
 #load gan model
-#gan = load_model('gan_model.h5')
+#gan = load_model('gan_model.keras')
 # Compile the GAN
 gan.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.0002, beta_1=0.5), metrics=['accuracy'])
 gan.summary()
@@ -112,31 +101,72 @@ gan.summary()
 
 # Optionally, save the generated deblurred images during training here
 #for epoch in range(16):
-d_loss_real = discriminator.fit(train_img, train_img, batch_size=16, epochs=1)
-d_loss_fake = discriminator.fit(train_blur, train_blur, batch_size=16, epochs=1)
-g_loss = gan.fit(train_img, train_img, batch_size=16, epochs=1)
+
+
+d_loss_real = discriminator.fit(train_img, train_img, batch_size=16, epochs=30)
+#d_loss_fake = discriminator.fit(x_train_noisy , x_train_noisy , batch_size=16, epochs=30)
+g_loss = gan.fit(train_img, train_img, batch_size=16, epochs=30)
 
 formatted_d_loss_real = "{:.6f}".format(d_loss_real.history['loss'][0])
-formatted_d_loss_fake = "{:.6f}".format(d_loss_fake.history['loss'][0])
+#formatted_d_loss_fake = "{:.6f}".format(d_loss_fake.history['loss'][0])
 formatted_g_loss = "{:.6f}".format(g_loss.history['loss'][0])
 
-print(f"\nDiscriminator Loss (Real): {formatted_d_loss_real} | Discriminator Loss (Fake): {formatted_d_loss_fake} | Generator Loss: {formatted_g_loss}")
+print(f"\nDiscriminator Loss (Real): {formatted_d_loss_real} | Generator Loss: {formatted_g_loss}")
+#print(f"\nDiscriminator Loss (Real): {formatted_d_loss_real} | Discriminator Loss (Fake): {formatted_d_loss_fake} | Generator Loss: {formatted_g_loss}")
 # Save the generator model
-generator.save('generator_model.h5')
+generator.save('generator_model.keras')
 # Save the discriminator model
-discriminator.save('discriminator_model.h5')
+discriminator.save('discriminator_model.keras')
 # Save the GAN model
-gan.save('gan_model.h5')
+gan.save('gan_model.keras')
 
-predictions = gan.predict(test_img)
-print(predictions.shape)
 
-for i in range(5):
-    plt.subplot(4, 5, i + 6)
-    plt.imshow(test_img[i, :, :, :]) #input
-    plt.subplot(4, 5, i + 11)
-    plt.imshow(test_blur[i, :, :, :]) #blur
-    plt.subplot(4, 5, i + 16)
-    plt.imshow(predictions[i, :, :, :]) #testing images
-
+plt.figure(figsize=(12, 10))
+plt.plot(d_loss_real.history['loss'])
+#plt.plot(d_loss_fake.history['accuracy'])
+plt.plot(g_loss.history['loss'])
+plt.title('Loss on Model')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['Real Loss', 'Fake Loss', 'GANs Loss'], loc='upper right')
 plt.show()
+
+
+for i in range(3):
+    blurred_images = test_imgs[i+18]
+    #de = test_imgs[i+18]
+    #blurred_images = cv2.imread('mini-project/test3.jpg')
+    #de = cv2.imread('mini-project/test3.jpg')
+    #blurred_images = cv2.cvtColor(blurred_images, cv2.COLOR_BGR2RGB)
+    #de = cv2.cvtColor(de, cv2.COLOR_BGR2RGB)
+    gray_image = cv2.cvtColor(blurred_images, cv2.COLOR_RGB2GRAY)
+    face_detect = face_model.detectMultiScale(gray_image)
+    plt.subplot(1, 2, 1)
+    plt.title('Original')
+    plt.imshow(test_imgs[i+18]) 
+
+    for (x,y,w,h) in face_detect:
+        #blur image
+        cv2.rectangle(blurred_images, (x,y), (x+w,y+h), (255,0,0), 2)
+        #print(f"{x}, {y}, {w}, {h}")
+        roi_color = blurred_images[y:y+h, x:x+w]
+        
+        roi_color = cv2.resize(roi_color, (128, 128))
+        blurred_face = gan.predict(np.expand_dims(roi_color, axis=0))
+        
+        img_h, img_w, _ = roi_color.shape
+        blurred_face = cv2.resize(blurred_face[0], (img_w, img_h))
+        print('----------')
+        print(blurred_images[y:y+img_h, x:x+img_w].shape)
+        print('----------')
+        print(blurred_face.shape)
+        print('----------')
+        blurred_images[y:y+img_h, x:x+img_w] = cv2.GaussianBlur(blurred_face, (23,23), 30)
+
+    #original_height, original_width = blurred_images.shape[:2]
+    #de = cv2.resize(de, (original_height*0.5, original_width*0.5))
+    #blurred_images = cv2.resize(blurred_images, (original_height*0.5, original_width*0.5))
+    plt.subplot(1, 2, 2)
+    plt.title('Blurred')
+    plt.imshow(blurred_images) #blur
+    plt.show()
--- mini-project/privface-autoencoder.py
+++ mini-project/privface-autoencoder.py
@@ -13,44 +13,32 @@ from matplotlib import pyplot as plt
 
 filenames = glob.glob("mini-project/archive/images/*.png")
 all_imgs = []
-blur_imgs = []
+test_imgs = []
 #model face detect
 face_model = cv2.CascadeClassifier('mini-project/face-detect-model.xml')
 
-for i in range(400):
+
+
+for i in range(len(filenames)):
     img = image.load_img(filenames[i],target_size=(128, 128, 3), interpolation="nearest")
     img = np.array(img)
     img = img/255
     all_imgs.append(img)
 
-    b_img = Image.open(filenames[i])
-    b_img = b_img.convert('RGB')
-    b_img = np.array(b_img)
-    
+    if len(all_imgs) > (70/100) * len(filenames):
+        b_img = Image.open(filenames[i])
+        b_img = b_img.convert('RGB')
+        b_img = np.array(b_img)
+        test_imgs.append(b_img)
 
-    gray_image = cv2.cvtColor(b_img, cv2.COLOR_BGR2GRAY)
-    face_detect = face_model.detectMultiScale(gray_image)
-    for (x,y,w,h) in face_detect:
-        #blur image
-        roi_color = b_img[y:y+h, x:x+w]
-        roi_b = cv2.GaussianBlur(roi_color, (23,23), 30)
-        b_img[y:y+h, x:x+w] = roi_b
-
-    b_img = cv2.resize(b_img,(128, 128))
-    #b_img = cv2.cvtColor(b_img, cv2.COLOR_BGR2RGB)
-    b_img = b_img/255
-    blur_imgs.append(b_img)
 
 
 all_imgs = np.array(all_imgs)
-blur_imgs = np.array(blur_imgs)
 
-
-train_img,test_img = train_test_split(all_imgs,random_state=32,test_size=0.3)
+train_img, _  = train_test_split(all_imgs,random_state=32,test_size=0.3)
 train_img,val_img = train_test_split(train_img,random_state=32,test_size=0.3)
 
-train_blur,test_blur = train_test_split(blur_imgs,random_state=32,test_size=0.3)
-train_blur,val_blur = train_test_split(train_blur,random_state=32,test_size=0.3)
+
 
 
 #Encoder
@@ -60,7 +48,7 @@ x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
 x = MaxPool2D( (2, 2))(x)
 encoded = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
 
-#decoder
+#Decoder
 x = Conv2D(64, (3, 3), activation='relu', padding='same')(encoded)
 x = UpSampling2D((2, 2))(x)
 x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
@@ -68,22 +56,22 @@ x = Conv2D(256, (3, 3), activation='relu', padding='same')(x)
 decoded = Conv2D(3, (3, 3), padding='same')(x)
 
 #autoencoder = Model(Input_img, decoded)
-autoencoder = load_model('face-blur-autoencoder_model.h5')
+autoencoder = load_model('face-blur-autoencoder_model.keras')
 autoencoder.compile(optimizer='adam', loss='mse')
 autoencoder.summary()
 
+"""
 callback = EarlyStopping(monitor='loss', patience=3)
-history = autoencoder.fit (train_img, train_blur,
-        epochs=1,
+history = autoencoder.fit (train_img, train_img,
+        epochs=30,
         batch_size=16,
         shuffle=True,
-        validation_data=(val_img, val_blur),
+        validation_data=(val_img, val_img),
         callbacks=[callback],verbose=1)
 
-autoencoder.save('face-blur-autoencoder_model.h5')
 
-#predictions = autoencoder.predict(test_img)
-predictions = autoencoder.predict(test_img)
+autoencoder.save('face-blur-autoencoder_model.keras')
+
 
 plt.figure(figsize=(12, 10))
 plt.plot(history.history['loss'])
@@ -94,15 +82,46 @@ plt.xlabel('epoch')
 plt.legend(['train', 'validate'], loc='upper left')
 plt.show()
 
-for i in range(5):
 
-    plt.subplot(4, 5, i + 6)
-    plt.imshow(test_img[i, :, :, :]) #input
-    plt.subplot(4, 5, i + 11)
-    plt.imshow(test_blur[i, :, :, :]) #blur
-    plt.subplot(4, 5, i + 16)
-    plt.imshow(predictions[i, :, :, :]) #testing images
+"""
+for i in range(3):
+    blurred_images = test_imgs[i+10]
+    #de = test_imgs[i+18]
+    #blurred_images = cv2.imread('mini-project/test3.jpg')
+    #de = cv2.imread('mini-project/test3.jpg')
+    #blurred_images = cv2.cvtColor(blurred_images, cv2.COLOR_BGR2RGB)
+    #de = cv2.cvtColor(de, cv2.COLOR_BGR2RGB)
+    plt.subplot(1, 2, 1)
+    plt.title('Original')
+    plt.imshow(test_imgs[i+10])
+    
+    gray_image = cv2.cvtColor(blurred_images, cv2.COLOR_RGB2GRAY)
+    face_detect = face_model.detectMultiScale(gray_image)
 
-plt.show()
-
-""""""
+    for (x,y,w,h) in face_detect:
+        #blur image
+        cv2.rectangle(blurred_images, (x,y), (x+w,y+h), (255,0,0), 2)
+        #print(f"{x}, {y}, {w}, {h}")
+        roi_color = blurred_images[y:y+h, x:x+w]
+        
+        roi_color = cv2.resize(roi_color, (128, 128))
+        blurred_face = autoencoder.predict(np.expand_dims(roi_color, axis=0))
+        
+        img_h, img_w, _ = roi_color.shape
+        blurred_face = cv2.resize(blurred_face[0], (img_w, img_h))
+        print('----------')
+        print(blurred_images[y:y+img_h, x:x+img_w].shape)
+        print('----------')
+        print(blurred_face.shape)
+        print('----------')
+        blurred_images[y:y+img_h, x:x+img_w] = cv2.GaussianBlur(blurred_face, (23,23), 30)
+    
+    #original_height, original_width = blurred_images.shape[:2]
+    #de = cv2.resize(de, (original_height*0.5, original_width*0.5))
+    #blurred_images = cv2.resize(blurred_images, (original_height*0.5, original_width*0.5))
+    
+    plt.subplot(1, 2, 2)
+    plt.title('Blurred')
+    plt.imshow(blurred_images) #blur
+    
+    plt.show()
