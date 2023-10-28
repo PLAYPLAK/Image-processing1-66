55ea64e5b4ec56bd392dea00130132353c5d7136
PLAYPLAK <131369587+PLAYPLAK@users.noreply.github.com>
2023-10-28 17:57:09 +0700

    ีupdate 28-10-2566

--- Lab6/Lab6-1.py
+++ Lab6/Lab6-1.py
@@ -4,11 +4,11 @@ from matplotlib import pyplot as plt
 from tensorflow.keras.models import Model
 from tensorflow.keras.applications import MobileNet
 from tensorflow.keras.applications.mobilenet import preprocess_input
+from tensorflow.keras.layers import Input, Conv2D, Flatten, Dense, Reshape, Conv2DTranspose, MaxPool2D, UpSampling2D, GlobalAveragePooling2D
 from keras.preprocessing.image import load_img, img_to_array, ImageDataGenerator
 from keras.utils import to_categorical
 from tensorflow.keras.optimizers import Adam
 from sklearn.metrics import confusion_matrix, classification_report
-
 from tensorflow.keras.applications.vgg16 import VGG16
 
 
@@ -16,9 +16,9 @@ from tensorflow.keras.applications.vgg16 import VGG16
 
 #Load base model
 base_model=MobileNet(weights='imagenet',include_top=False, input_shape=(224, 224, 3))
+
 # Add new layers
 x=base_model.output
-
 x=GlobalAveragePooling2D()(x)
 x=Dense(1024,activation='relu')(x) # dense layer 1
 x=Dense(1024,activation='relu')(x) #dense layer 2
@@ -27,4 +27,14 @@ preds=Dense(3,activation='softmax')(x)
 
 model=Model(inputs=base_model.input,outputs=preds)
 
+for layer in model.layers[:86]:
+    layer.trainable=False #Freeze base model
+for layer in model.layers[86:]:
+    layer.trainable=True
+
 model.summary()
+"""
+for i, layer in enumerate(base_model.layers):
+    print(i, layer.name)
+
+"""
